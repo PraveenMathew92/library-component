e2e1efe8078e8715a228a72f055dd4cb5eb5beb9
Praveen Mathew <mpraveen@thoughtworks.com>
2018-09-09 18:12:00 +0530

    add title bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import LeftNav from './LeftNav';
+import TitleBar from './TitleBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TitleBar />
         <LeftNav />
       </div>
     );
---
+++ src/components/TitleBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/TitleBar.css';
+
+const TitleBar = () => (
+    <div className='titleBar'>
+        Library
+        </div>
+);
+
+export default TitleBar;
