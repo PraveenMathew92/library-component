7e5052a0b917bfd1472fc80dc2a3b216a3a699cd
Praveen Mathew <mpraveen@thoughtworks.com>
2018-09-10 21:03:01 +0530

    add page to accept reader id

---
+++ src/components/AcceptReaderId.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const AcceptReaderId = () => (
+    <div>
+        <input placeholder="Reader Id" />
+        <br />
+        <button type="submit">SUBMIT</button>
+    </div>
+);
+
+export default AcceptReaderId;
