3c0fdd84eb0364f7873ba26073696ae476f15e15
Praveen Mathew <mpraveen@thoughtworks.com>
2018-09-10 10:10:29 +0530

    add search bar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const HomePage = () => (
     <div>
-        Search Box
+        <input placeholder="Book Name"></input>
+        <button type="submit">SEARCH</button>
     </div>
 );
 
