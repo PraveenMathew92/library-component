d627ed4279362efb2f1f67e3e85c758fa397c036
Praveen Mathew <mpraveen@thoughtworks.com>
2018-09-14 02:03:20 +0530

    add Admin View

--- src/components/AdminView.js
+++ src/components/AdminView.js
@@ -1,2 +1,30 @@
 import React from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
+const AdminView = () => (
+    <Tabs>
+      <TabList>
+        <Tab>ADD READER</Tab>
+        <Tab>ADD BOOK</Tab>
+      </TabList>
+
+      <TabPanel>
+          <div>
+              <input> NAME </input>
+              <input> ADDRESS </input>
+              <input type="submit"> ADD </input>
+          </div>
+      </TabPanel>
+      <TabPanel>
+          <div>
+              <input> TITLE </input>
+              <input> ISBN </input>
+              <input> AUTHOR </input>
+              <input> No of Copies </input>
+              <input type="submit"> ADD </input>
+          </div>
+      </TabPanel>
+    </Tabs>
+  );
+
+  export default AdminView;
