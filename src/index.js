24f67972f51dbae33fd264ee10ecc4f4480c8adb
Praveen Mathew <mpraveen@thoughtworks.com>
2018-09-16 14:47:18 +0530

    render component based on route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import AdminView from './components/AdminView';
 import HomePage from './components/HomePage';
+import ProfilePage from './components/ProfilePage';
 
-ReactDOM.render(<App><HomePage /></App>, document.getElementById('root'));
+ReactDOM.render(<App>
+    <BrowserRouter>
+    <Switch>
+        <Route path={'/admin'} component={AdminView}/>
+        <Route path={'/profile'} component={ProfilePage}/>
+        <Route path={'/'} component={HomePage}/>
+    </Switch>
+    </BrowserRouter>
+    </App>, document.getElementById('root'));
