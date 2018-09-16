6612eb43e4dcdb5940d9a0d815a646920b34a234
Praveen Mathew <mpraveen@thoughtworks.com>
2018-09-16 14:44:48 +0530

    add profile page

---
+++ src/components/ProfilePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProfilePage = () => (
+    <div>
+        NAME: JACK REACHER<br/>
+        ADDRESS: MOTEL<br/>
+        DOB: 11 SEPT 1965<br/>
+        BOOKS: 
+    </div>
+);
+
+export default ProfilePage;
