d42c000a58934f90f3d328b657e41155df022dba
Praveen Mathew <mpraveen@thoughtworks.com>
2018-09-19 21:51:03 +0530

    add routing to book tiles

--- src/components/BookTiles.js
+++ src/components/BookTiles.js
@@ -3,7 +3,7 @@ import "../css/BookTiles.css";
 import BookDetails from './BookDetails';
 
 const BookTiles = (props) => (
-    <div className="bookTiles" onClick={() => <App><BookDetails /></App>}>
+    <div className="bookTiles" onClick="/">
             NAME: {props.name}<br />
             AUTHOR: {props.author}<br />
             ISBN: {props.isbn}<br />
